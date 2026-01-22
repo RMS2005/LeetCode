fe7b4c379ed76a44bb6e94dbfd1029284b575f74
RMS2005 <167501438+RMS2005@users.noreply.github.com>
2026-01-22 21:09:52 +0530

    Leetcode(70)

---
+++ Easy/Climbing_stairs.cpp
@@ -0,0 +1,44 @@
+/* You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+ 
+
+Constraints:
+
+1 <= n <= 45*/
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==1) return 1;
+        if(n==2) return 2;
+        int p1 = 1;
+        int p2 = 2;
+
+         for(int i=3;i<=n;i++)
+         {
+             int steps = p1+p2;
+             p1 = p2;
+             p2 = steps;
+         }
+        return p2;
+    }
+};
